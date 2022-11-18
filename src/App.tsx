83c07cf2400145f23340d629c9f39c8c22c7f6c1
Prajwal Gautam <prajwalgautam10@gmail.com>
2022-11-18 16:44:08 +0545

    working odometer good design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Odometer from 'react-odometerjs';
 
 function App() {
+  const [values, setValues] = useState<Array<number>>([0,0,0,0,0,0,0,0,0,0]);
+  const [desiredNum, setDesiredNum] = useState([9,8,4,0,4,3,3,4,5,6]);
+
+  const handleOnClick = () => {
+    if(JSON.stringify(values) === JSON.stringify(desiredNum)){
+      setValues([0,0,0,0,0,0,0,0,0,0])
+      return;
+    }else{
+      for(let i=1; i<=desiredNum.length; i++){
+        const timeout = 2000*(i-1);
+        setTimeout(() => {
+          let newValue = [...desiredNum.slice(0, i), ...values.slice(i)];
+          setValues(newValue);
+        }, timeout);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <h1> Hello Boss </h1>
-      <Odometer value={1234} format="(.ddd),dd" />;
+      
+      <div className='container'>
+        <div>
+        {
+          values.map((value, index) => (
+            <Odometer
+              key={index}
+              format="dddd-ddd-ddd"
+              duration={2000}
+              value={value}
+              theme="custom"
+            />
+          ) )
+        }
+        </div>
+
+        <button onClick={handleOnClick}>Change</button>
+      </div>
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'odometer/themes/odometer-theme-train-station.css';
+import "./odometer-style.css";
 import './index.css';
 import App from './App';
 
