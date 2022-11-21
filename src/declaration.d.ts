6000790da570d6e87313df1a2c52480118a972a1
Prajwal Gautam <prajwalgautam10@gmail.com>
2022-11-21 13:52:40 +0545

    some thing new

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -3,3 +3,4 @@ declare module "*.svg";
 declare module "*.png";
 declare module "styled-components";
 declare module "react-odometerjs";
+declare module "react-icons";
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,12 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import CountDown from "../components/Counter";
 import Lottery from "../pages/lottery/Lottery";
 import { Upload, Proceed } from "../pages/upload/Upload";
+import WelcomePage from "../pages/welcomepage/WelcomePage";
 
 const Router = () => {
     return(
         <>
             <Routes>
-                <Route path="/" element={<div>home</div>} />
+                <Route path="/" element={<WelcomePage/>} />
                 <Route path="/next" element={<Upload/>} />
                 <Route path="/lottery-start" element={<Proceed/>} />
                 <Route path="/lottery" element={<Lottery/>} />
